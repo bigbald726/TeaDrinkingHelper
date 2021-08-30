3d17e5a90e83acf506eeb7ac45fcfb85faab44d7
bigbald726 <60995153+bigbald726@users.noreply.github.com>
2021-08-31 02:28:25 +0800

    adding another alarm to notify you go home!

--- drinkTea/UserInterface.py
+++ drinkTea/UserInterface.py
@@ -17,9 +17,12 @@ class Clock:
         self.root.title("饮茶小助手 v1.0")
         self.root.geometry('640x320')
         self.clockDisplay = Label(self.root, font=('calibri', 40, 'bold'), background='green', foreground='white')
-        self.hourInput = Spinbox(self.root, from_=0, to=23, font=('calibri', 20), width=3, justify=CENTER)
-        self.minuteInput = Spinbox(self.root, from_=0, to=59, font=('calibri', 20), width=3, justify=CENTER)
-        self.secondInput = Spinbox(self.root, from_=0, to=59, font=('calibri', 20), width=3, justify=CENTER)
+        self.teaHourInput = Spinbox(self.root, from_=0, to=23, font=('calibri', 20), width=3, justify=CENTER)
+        self.teaMinuteInput = Spinbox(self.root, from_=0, to=59, font=('calibri', 20), width=3, justify=CENTER)
+        self.teaSecondInput = Spinbox(self.root, from_=0, to=59, font=('calibri', 20), width=3, justify=CENTER)
+        self.workHourInput = Spinbox(self.root, from_=0, to=23, font=('calibri', 20), width=3, justify=CENTER)
+        self.workMinuteInput = Spinbox(self.root, from_=0, to=59, font=('calibri', 20), width=3, justify=CENTER)
+        self.workSecondInput = Spinbox(self.root, from_=0, to=59, font=('calibri', 20), width=3, justify=CENTER)
 
         # # rowconfigure for atheistic purpose only
         self.root.rowconfigure(0, weight=1)
@@ -36,15 +39,26 @@ class Clock:
         Label(self.root, text="Hour").grid(column=0, row=1)
         Label(self.root, text="Minute").grid(column=2, row=1)
         Label(self.root, text="Second").grid(column=4, row=1)
-        self.hourInput.grid(column=1, row=1)
-        self.minuteInput.grid(column=3, row=1)
-        self.secondInput.grid(column=5, row=1)
+        self.teaHourInput.grid(column=1, row=1)
+        self.teaMinuteInput.grid(column=3, row=1)
+        self.teaSecondInput.grid(column=5, row=1)
 
         confirmButton = Button(self.root, text='准备饮茶', command=self.prepareMilkTea)
         confirmButton.grid(column=6, row=1, sticky=E)
         confirmButton = Button(self.root, text='还是算了')
         confirmButton.grid(column=7, row=1)
 
+        Label(self.root, text="Hour").grid(column=0, row=2)
+        Label(self.root, text="Minute").grid(column=2, row=2)
+        Label(self.root, text="Second").grid(column=4, row=2)
+        self.workHourInput.grid(column=1, row=2)
+        self.workMinuteInput.grid(column=3, row=2)
+        self.workSecondInput.grid(column=5, row=2)
+
+        confirmButton = Button(self.root, text='我要放工', command=self.prepareGetOffWork)
+        confirmButton.grid(column=6, row=2, sticky=E)
+        confirmButton = Button(self.root, text='还是算了')
+        confirmButton.grid(column=7, row=2)
 
     def run(self):
         self.userInput()
@@ -52,12 +66,22 @@ class Clock:
         mainloop()
 
     def prepareMilkTea(self):
-        hour = self.hourInput.get()
-        minute = self.minuteInput.get()
-        second = self.secondInput.get()
+        hour = self.teaHourInput.get()
+        minute = self.teaMinuteInput.get()
+        second = self.teaSecondInput.get()
         drinkTeaAlarm = Alarm(hour, minute, second)
         alertAnime = Animation()
         alertAnime.musicURL = "src/sound/yumcha.wav"
+        drinkTeaAlarm.AlarmClock()
+        alertAnime.run()
+
+    def prepareGetOffWork(self):
+        hour = self.workHourInput.get()
+        minute = self.workMinuteInput.get()
+        second = self.workSecondInput.get()
+        drinkTeaAlarm = Alarm(hour, minute, second)
+        alertAnime = Animation()
+        alertAnime.musicURL = "src/sound/2000timeout.wav"
         drinkTeaAlarm.AlarmClock()
         alertAnime.run()
 
